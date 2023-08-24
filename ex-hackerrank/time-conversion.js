03ce9473bb7388d6de8b90bf985f51fd97604230
Pastel <gaitadothiago@gmail.com>
2023-08-23 21:17:36 -0300

    Fixing the time-conversion exercise, using split/parseint/if/else

--- ex-hackerrank/time-conversion.js
+++ ex-hackerrank/time-conversion.js
@@ -1,9 +1,25 @@
 function timeConversion(s) {
 
+  let splited = s.split('')
+  let hours = parseInt(splited[0] + splited[1])
+  let minutes = splited[3] + splited[4]
+  let seconds = splited[6] + splited[7]
 
+  if(hours >= 12 && splited[8] === "A") {
+    hours = hours - 12
+  } else if(hours < 12 && splited[8] === "P") {
+    hours += 12
+  }
+
+
+  if(hours < 10) {
+    return `0${hours}:${minutes}:${seconds}`
+  } else {
+    return `${hours}:${minutes}:${seconds}`
+  }
 
 }
 
-example = '12:40:22AM'
+example2 = '04:59:59AM'
 
-timeConversion(example)
+console.log(timeConversion(example2))
